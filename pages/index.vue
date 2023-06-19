40266e7fa7ef0ef722b4c234dc93469607aeda02
Nathan <nathancai1031@gmail.com>
2023-06-19 13:16:58 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hello World</h1>
+  <NuxtLink to="?id=1">Link to id 1</NuxtLink>
+  <NuxtLink to="?id=2">Link to id 2</NuxtLink>
+  <NuxtLink to="?id=3">Link to id 3</NuxtLink>
+</template>
+<script setup>
+const route = useRoute();
+console.log(route.fullPath);
+console.log(route.query.id);
+</script>
